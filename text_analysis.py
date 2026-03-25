dd5b8254e87fa0b052a0500251a94f6316a90d85
Erzen Citaku <erzencitaku@gmail.com>
2026-03-25 16:12:55 +0100

    Text Analysis task

---
+++ text_analysis.py
@@ -0,0 +1,17 @@
+# text = "Python is powerful and python helps build powerful applications"
+# Write code to:
+# •	Count occurrences of each word 
+# •	Return results as a dictionary 
+# •	Treat uppercase and lowercase as the same
+
+text = "Python is powerful and python helps build powerful applications"
+def count_words(text):
+    word_count = {}
+    words = text.lower().split() #Kthimi i tekstit ne lowercase dhe ndarja e fjaleve
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1 #Rritja e numrit te fjales nese ekziston
+        else:
+            word_count[word] = 1 #Shtimi i fjales ne dictionary me vleren 1
+    return word_count
+print("Numri i fjaleve:", count_words(text))

2a6f7b9e4ffb954c1115cba38c4ca3d47f6ee9f6
Erzen Citaku <erzencitaku@gmail.com>
2026-03-25 12:39:33 +0100

    Task 1 - Validating Deliveries

--- deliveries.py
+++ deliveries.py
@@ -1,1 +1,19 @@
 print("Homework Task 1: Deliveries")
+
+#Delieveries list with some deliveries having negative distance and some pending deliveries
+deliveries = [
+    {"delivery_id": 1, "distance": 12, "status": "completed"},
+    {"delivery_id": 2, "distance": -3, "status": "completed"},
+    {"delivery_id": 3, "distance": 8, "status": "pending"},
+    {"delivery_id": 4, "distance": 20, "status": "completed"}
+]
+
+#Funksioni per filtrimin e deliveries, duke shfaqur vetem ato qe kan statusin "completed" dhe distance pozitive
+def validimi(data):
+    valid_deliveries = []
+    for d in data:
+        if d["distance"] >= 0 and d["status"] == "completed":
+            valid_deliveries.append(d)
+    return valid_deliveries
+
+print("Deliveries valide:", validimi(deliveries))
